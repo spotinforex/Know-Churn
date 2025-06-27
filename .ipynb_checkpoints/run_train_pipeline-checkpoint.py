837bc0cbbfb9e7353fb70c762be4b10725e18355
spotinforex <nwabekepraisejah@gmail.com>
2025-06-27 22:04:33 +0100

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/run_train_pipeline-checkpoint.py
+++
@@ -1,7 +0,0 @@
-from pipelines.training_pipeline import train_pipeline
-
-file_path_csv = "C:/Users/SPOT/Documents/Customer_Churn/data/telco_churn_with_all_feedback.csv"
-
-if __name__ == "__main__":
-    # Run the pipeline
-    metric = train_pipeline(data_path = file_path_csv)
